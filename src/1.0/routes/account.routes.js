f9aaa39c9e4b7058a6279ae005996dae60971a89
Antoine ROBIN <antoine.robin@factorfx.com>
2024-04-04 15:02:20 +0200

    refactor(service): change service name

--- src/1.0/routes/account.routes.js
+++ src/1.0/routes/account.routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 
-const authController = require("../controllers/authentication.controllers")
+const authController = require("../controllers/account.controllers")
 
 router.get("/", (req, res) => {
     res.status(200).json({ message: "API is up!" })

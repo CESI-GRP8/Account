ebb94d42aba5ee33f83e11d429109c87276173c2
Solliffan <114048253+Solliffan@users.noreply.github.com>
2024-04-10 17:35:13 +0200

    cors fix

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-require("dotenv").config()
-const dbClient = require("./src/config/database")
-const express = require("express")
+require("dotenv").config();
+const dbClient = require("./src/config/database");
+const express = require("express");
+const cors = require("cors");
 
-dbClient()
+dbClient();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/1.0/accounts", require("./src/1.0/routes/account.routes"))
+// Middleware pour gérer les requêtes OPTIONS
+app.options("/1.0/accounts/register", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  res.sendStatus(200);
+});
 
-app.listen(process.env.APP_PORT, process.env.APP_URL, () => console.log(`API server started on ${process.env.APP_URL}:${process.env.APP_PORT}`))
+app.use("/1.0/accounts", require("./src/1.0/routes/account.routes"));
+
+app.listen(process.env.APP_PORT, process.env.APP_URL, () =>
+  console.log(`API server started on ${process.env.APP_URL}:${process.env.APP_PORT}`)
+);

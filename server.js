27aad19031282ee09ab4b3d3402053ca9e41d25b
Antoine ROBIN <antoine.robin@factorfx.com>
2024-04-11 21:11:31 +0200

    feat(swagger): add swagger

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config()
 const dbClient = require("./src/config/database")
 const express = require("express")
 const cors = require("cors")
+const swaggerUI = require("swagger-ui-express")
+const swaggerJSDoc = require("swagger-jsdoc")
 
 dbClient()
 
@@ -10,6 +12,22 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Account API",
+            version: "1.0.0",
+            description: "Here is the documentation of the account micro service."
+        },
+    },
+    apis: ["./src/1.0/routes/account.routes.js"],
+}
+
+const specs = swaggerJSDoc(options)
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
 app.use("/1.0/accounts", require("./src/1.0/routes/account.routes"))
 
